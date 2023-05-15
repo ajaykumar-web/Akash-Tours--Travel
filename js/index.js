29beb6a7ed2bc7a5a2f7c1dd6392daf6079b2322
ajaykumar-web <ajay253060@gmail.com>
2023-05-15 18:31:51 +0530

    testimonial

--- js/index.js
+++ js/index.js
@@ -6,31 +6,3 @@ $(window).scroll(function() {
         $(".navbar").removeClass("header-dark");
     }
 });
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    autoplay: false,
-    margin:-20,
-    loop: true,
-    nav:true,
-    navText: ['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'
-  ],
-    responsive: {
-      0: {
-        items: 1,
-        nav:false,
-      },
-  
-      600: {
-        items: 1.5,
-        nav:false,
-      },
-      768: {
-        items: 1.5,
-      },
-      1140: {
-        items:2.2,
-      },
-    }
-
-  });
-});
